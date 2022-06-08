f314b36e8de2657b7e5221be162dddbc8c348eca
jose <4417780+jos101@users.noreply.github.co>
2022-06-08 11:23:43 -0500

    Added urls

--- mlgetpy/search.py
+++ mlgetpy/search.py
@@ -1,3 +1,11 @@
+import mechanicalsoup
+
 def search():
 
+    browser = mechanicalsoup.StatefulBrowser()
+    url = "https://archive-beta.ics.uci.edu/ml/datasets"
+    url = "https://archive.ics.uci.edu/ml/datasets.php"
+
+    browser.open(url)
+    print(browser.get_current_page())
     return "searching..."
