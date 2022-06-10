21114fc813895ac0a308684174f6bf6b00b571a6
AR1 <4417780+jos101@users.noreply.github.com>
2022-06-10 12:31:01 -0500

    added Repository class

---
+++ mlgetpy/Repository.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataSetList:
+    
+    def find():
+        NotImplemented
+    
+    def download():
+        NotImplemented
+    
+    def remove():
+        NotImplemented
