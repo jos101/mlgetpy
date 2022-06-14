aaff02798be2fd3988bdb60b25f0e57eb2476424
4417780+jos101@users.noreply.github.com <4417780+jos101@users.noreply.github.com>
2022-06-14 11:29:50 -0500

    added fields

--- mlrgetpy/DataFrameConverter.py
+++ mlrgetpy/DataFrameConverter.py
@@ -7,20 +7,6 @@ class DataFrameConverter:
     def convertFromList(self, rows: list) -> pd.DataFrame:
         dict = {} 
 
-        ID = []
-        userID = []
-        introPaperID  = []
-        Name  = []
-        Abstract  = []
-        Area  = []
-        Task  = []
-        Types  = []
-        DOI  = []
-        DateDonated  = []
-        isTabular  = []
-        URLFolder  = []
-        URLReadme  = []
-        URLLink  = []
         Graphics  = []
         Status  = []
         NumHits  = []
@@ -28,7 +14,6 @@ class DataFrameConverter:
         numInstances  = []
         slug  = []
         
-
         dict["ID"] = []
         dict["userID"] = []
         dict["introPaperID"] = []
@@ -36,20 +21,54 @@ class DataFrameConverter:
         dict["Abstract"] = []
         dict["Area"] = []
         dict["Task"] = []
+        dict["Types"] = []
+
+        dict["DOI"] = []
+        dict["DateDonated"] = []
+
+        dict["isTabular"] = []
+        dict["URLFolder"] = []
+        dict["URLReadme"] = []
+        dict["URLLink"] = []
+
+        dict["Graphics"] = []
+        dict["Status"]  = []
+        dict["NumHits"] = []
+        dict["AttributeTypes"] =  []
+        dict["numInstances"] = []
+        dict["slug"] = []
+
+        dict["tabular"] = []
+        dict["user"] = []
 
         for i in rows:
-            dict["ID"].append(i["ID"])
-            dict["userID"].append(i["userID"])
-            dict["introPaperID"].append(i["introPaperID"])
-            dict["Name"].append(i["Name"])
+            dict["ID"].append( i["ID"] )
+            dict["userID"].append( i["userID"] )
+            dict["introPaperID"].append( i["introPaperID"] )
+            dict["Name"].append( i["Name"] )
 
-            dict["Abstract"].append(i["Abstract"])
-            dict["Area"].append(i["Area"])
+            dict["Abstract"].append( i["Abstract"] )
+            dict["Area"].append( i["Area"] )
 
-            dict["Task"].append(i["Task"])
+            dict["Task"].append( i["Task"] )
+            dict["Types"].append( i["Types"] )
+            dict["DOI"].append( i["DOI"] )
+            dict["DateDonated"].append( i["DateDonated"] )
 
+            dict["isTabular"].append( i["isTabular"] )
+            dict["URLFolder"].append( i["URLFolder"] )
+            dict["URLReadme"].append( i["URLReadme"] )
+            dict["URLLink"].append( i["URLLink"] )
 
-        
+            dict["Graphics"].append( i["Graphics"] )
+            dict["Status"].append( i["Status"] )
+            dict["NumHits"].append( i["NumHits"] )
+            dict["AttributeTypes"].append( i["AttributeTypes"] )
+            dict["numInstances"].append( i["numInstances"] ) 
+            dict["slug"].append( i["slug"] )    
+
+            dict["tabular"].append( i["tabular"] )    
+            dict["user"].append( i["user"] )    
 
         df = pd.DataFrame.from_dict(dict)
 
